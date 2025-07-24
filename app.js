9329bd70237895cfc48c53c589c410f820a69528
kukushastik <kukushastiki@gmail.com>
2025-07-25 01:20:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const { sequelize } = require('./src/models');
 const authRoutes = require('./src/api/auth.routes');
 
 const app = express();
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use('/auth', authRoutes);
 
@@ -17,6 +19,9 @@ const path = require('path');
 app.use('/upload', uploadRoutes);
 app.use('/admin', express.static(path.join(__dirname, 'src/views')));
 const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
