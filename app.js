3d33083c4fb79de2705a0bc697df5130365f47e7
kukushastik <kukushastiki@gmail.com>
2025-07-25 01:08:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -16,3 +16,7 @@ const path = require('path');
 
 app.use('/upload', uploadRoutes);
 app.use('/admin', express.static(path.join(__dirname, 'src/views')));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
